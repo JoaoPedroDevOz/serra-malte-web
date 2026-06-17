7e23db2027a7c074de636e77a78ac45b52e108f3
Maylon Ferreira <maylonferreira18@outlook.com>
2026-06-17 12:26:43 -0300

    Arrumando login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext";
 import LandingPage from "./pages/landingPage/LandingPage";
-import Login from "./pages/login/Login";
-import Admin from "./pages/admin/Admin";
-import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <AuthProvider>
       <LandingPage />
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/admin"
-            element={
-              <PrivateRoute>
-                <Admin />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter> */}
     </AuthProvider>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,30 +3,49 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import AdminPage from "./pages/adminPage/adminPage";
+import Login from "./pages/login/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider } from "./Context/AuthContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: "/home",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: (
+      <AuthProvider>
+        <Login />
+      </AuthProvider>
+    ),
+  },
+  {
+    path: "/admin",
+    element: (
+      <AuthProvider>
+        <PrivateRoute>
+          <AdminPage />
+        </PrivateRoute>
+      </AuthProvider>
+    ),
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    path: '/'
-  },
-  {
-    element: <AdminPage />,
-    path: '/admin'
-  },
-]);
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
